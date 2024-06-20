8deea7b8fcbb05c371041eb07788c1976fa54971
trungvinh02 <work.trungvinh@gmail.com>
2024-06-20 10:56:55 +0700

    update

--- src/shared/constants/app.js
+++ src/shared/constants/app.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://mobile-shop-backend-nodejs.onrender.com/";
+export const BASE_URL = "https://mobile-shop-backend-nodejs.onrender.com";
 export const BASE_API = "https://mobile-shop-backend-nodejs.onrender.com/api/v1";
